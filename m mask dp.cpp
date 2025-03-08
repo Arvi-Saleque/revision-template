6b4d7ebf4b669712528836b23b4aa7e077e86e88
Arvi_Saleque <alifsalek.as@gmail.com>
2025-03-09 00:38:13 +0600

    m mask dp.cpp

--- m mask dp.cpp
+++ m mask dp.cpp
@@ -1,1 +1,26 @@
-
+// m-mask dp
+vector<map<ll, ll>> dp;
+ll f(int pos, ll mask) { 
+    if(pos == n) {
+        vector<int> v(n);
+        ll tmpmask = mask;
+        for(int i = n - 1; i >= 0; i--) {
+            v[i] = (tmpmask % (m + 1));
+            tmpmask /= (m + 1); 
+        }
+        ll tmp = 0;
+        for(auto [a, b, c, d] : Q) {
+            tmp += (v[b] - v[a] == c ? d : 0);
+        }
+        return tmp;
+    }
+    if(dp[pos].count(mask)) return dp[pos][mask];
+    ll &ans = dp[pos][mask];
+    int start = 1;
+    if(pos > 0) start = mask % (m + 1);
+    for(int j = start; j <= m; j++) {
+        ll tmpmask = (mask * (m + 1) + j);
+        ans = max(ans, f(pos + 1, tmpmask));
+    }
+    return ans;
+}
