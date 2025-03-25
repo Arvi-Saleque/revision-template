ce7f4325c8068531178f0ef77c31b4ca52ebbd29
Arvi_Saleque <alifsalek.as@gmail.com>
2025-03-25 19:27:18 +0600

    Update equation trick.cpp

--- equation trick.cpp
+++ equation trick.cpp
@@ -5,42 +5,8 @@
 // then try to generanize and make all i/j in one side
 // do some algebra and try to find solution
 
-```latex
-We start with the given equation:
-$$
-(a_i + a_j)(a_i^2 + a_j^2) \equiv k \pmod{p}.
-$$
+`a_i^4 - a_j^4 = (a_i - a_j)(a_i + a_j)(a_i^2 + a_j^2)`
 
-Observe that the difference of fourth powers factors as:
-$$
-a_i^4 - a_j^4 = (a_i - a_j)(a_i + a_j)(a_i^2 + a_j^2).
-$$
-
-Since \(a_i \neq a_j\) (and thus \(a_i - a_j \not\equiv 0 \pmod{p}\)), we can divide both sides by \(a_i - a_j\) (in modulo \(p\)) to get:
-$$
-\frac{a_i^4 - a_j^4}{a_i - a_j} \equiv k \pmod{p}.
-$$
-
-Multiplying both sides by \((a_i - a_j)\) leads to:
-$$
-a_i^4 - a_j^4 \equiv k(a_i - a_j) \pmod{p}.
-$$
-
-Rearrange the equation to group terms with \(a_i\) and \(a_j\) separately:
-$$
-a_i^4 - k\,a_i \equiv a_j^4 - k\,a_j \pmod{p}.
-$$
-
-This motivates the definition of the function:
-$$
-f(x) = x^4 - kx \pmod{p}.
-$$
-
-Thus, the condition for a valid pair \((i,j)\) is equivalent to:
-$$
-f(a_i) \equiv f(a_j) \pmod{p}.
-$$
-```
 
 #include<bits/stdc++.h>
 using namespace std;
