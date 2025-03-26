ac457a939ce2521f2b82bb12ba317c8780022747
Arvi_Saleque <alifsalek.as@gmail.com>
2025-03-27 00:01:37 +0600

    Update combi practce.cpp

--- combi practce.cpp
+++ combi practce.cpp
@@ -95,3 +95,68 @@ int main() {
     }
     return 0;
 }
+
+// https://atcoder.jp/contests/abc167/tasks/abc167_e?lang=en
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int const N = 2e5 + 2, mod = 998244353;
+int f[N], invf[N];
+ll power(ll a, ll b) {
+    ll ans = 1;
+    bool f = 1;
+    while(b) {
+        if(b & 1) {
+            ans *= a;
+            if(ans >= mod) {
+                ans %= mod;
+                f = 1;
+            }
+        }
+        a *= a;
+        if(a >= mod) {
+            a %= mod;
+            f = 1;
+        }
+        b >>= 1;
+    }
+    return (ans + (mod * f)) % mod;
+}
+void pre() {
+    f[0] = 1;
+    for (int i = 1; i < N; i++) {
+        f[i] = 1LL * i * f[i - 1] % mod;
+    }
+    invf[N - 1] = power(f[N - 1], mod - 2);
+    for (int i = N - 2; i >= 0; i--) {
+        invf[i] = 1LL * invf[i + 1] * (i + 1) % mod;
+    }
+}
+int nCr(int n, int r) {
+    if(r < 0 || r > n) return 0;
+    if(n == 0 && r == 0) return 1;
+    return 1LL * f[n] * invf[r] % mod * invf[n - r] % mod;
+}
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    ll ans = 0;
+    for(int i = 0; i <= k; i++) {
+        ll val = (1LL * nCr(n - 1, i) * power(m - 1, n - 1 - i)) % mod;
+        ans = (ans + val) % mod;
+    }
+    cout << (1LL * ans * m) % mod << "\n";
+}   
+int main() {
+    pre();
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1, cs = 1;
+    //cin >> t; 
+    while(t--) {
+        // /cout << "Case " << cs++ << ": ";
+        solve();
+    }
+    return 0;
+}
