d3123b22d87f294ddcefd1263819a06a369f1da3
Arvi_Saleque <alifsalek.as@gmail.com>
2025-05-15 16:04:05 +0600

    Update trie trick.cpp

--- trie trick.cpp
+++ trie trick.cpp
@@ -121,7 +121,6 @@ int main(){
     }
 } 
 
-
 // https://www.spoj.com/problems/SUBXOR/
 
 /*
@@ -201,6 +200,125 @@ int main(){
     cin.tie(0);
     int tc = 1, cs = 1;
     cin >> tc;
+    while(tc--) {
+        //cout << "Case " << cs++ << ": " << "\n";
+        solve();
+    }
+} 
+
+
+// https://vjudge.net/problem/CSAcademy-xor-submatrix
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1e7 + 9, mod = 998244353; 
+ 
+struct Trie {
+    const int B = 29;
+    struct node { // 1 based
+        node *child[2];
+        node() {
+            child[0] = child[1] = 0;
+        }
+    } *root;
+    
+    Trie () {
+        root = new node();
+    }
+
+    void insert(int x) {
+        auto cur = root;
+        for(int i = B - 1; i >= 0; i--) {
+            int id = (x >> i) & 1;
+            if(!cur->child[id]) cur->child[id] = new node();
+            cur = cur->child[id];
+        }
+    }
+
+    int get_max(int x) {
+        auto cur = root;
+        int ans = 0;
+        for(int i = B - 1; i >= 0; i--) {
+            int id = (x >> i) & 1;
+            if(cur->child[!id]) {
+                ans += (1 << i);
+                cur = cur->child[!id];
+            }
+            else if(cur->child[id]){
+                cur = cur->child[id];
+            }
+            else break;
+        }
+        return ans;
+    }
+
+    void clear(node *cur) {
+        if(!cur) return;
+        for(int i = 0; i < 2; i++) {
+            if(cur->child[i]) clear(cur->child[i]);
+        }
+        delete(cur);
+    }
+
+    ~Trie() {
+        clear(root);
+    }
+};
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(n + 1), u(m + 1);
+    vector<int> prev(n + 1), preu(m + 1);
+    for(int i = 1; i <= n; i++) {
+        cin >> v[i];
+        prev[i] = prev[i - 1] ^ v[i];
+    }
+    for(int j = 1; j <= m; j++) {
+        cin >> u[j];
+        preu[j] = preu[j - 1] ^ u[j];
+    }
+
+    int ans = 0;
+    vector<int> rowodd, colodd;
+    for(int i = 1; i <= n; i++) {
+        for(int j = i; j <= n; j++) {
+            int val = prev[j] ^ prev[i - 1];
+            if((j - i + 1) & 1) {
+                rowodd.push_back(val);
+            }
+            else {
+                ans = max(ans, val);
+            }
+        }
+    }
+    for(int i = 1; i <= m; i++) {
+        for(int j = i; j <= m; j++) {
+            int val = preu[j] ^ preu[i - 1];
+            if((j - i + 1) & 1) {
+                colodd.push_back(val);
+            }
+            else {
+                ans = max(ans, val);
+            }
+        }
+    }
+    Trie trie;
+    for(auto x : rowodd) {
+        trie.insert(x);
+    }
+    for(auto x : colodd) {
+        ans = max(ans, trie.get_max(x));
+    }
+    cout << ans << "\n";
+}  
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tc = 1, cs = 1;
+    //cin >> tc;
     while(tc--) {
         //cout << "Case " << cs++ << ": " << "\n";
         solve();
