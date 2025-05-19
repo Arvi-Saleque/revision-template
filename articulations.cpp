2210e6b79f8ce481da8280eb089583805f1fc702
Arvi_Saleque <alifsalek.as@gmail.com>
2025-05-20 02:34:04 +0600

    Update articulations.cpp

--- articulations.cpp
+++ articulations.cpp
@@ -84,3 +84,29 @@ int main() {
     }
     return 0;
 }
+
+int time = 0;
+        vector<bool> vis(n + 1);
+        vector<int> disc(n + 1), low(n + 1);
+        set<string> ans;
+        function<void(int, int)> dfs1 = [&](int u, int p) -> void {
+            vis[u] = 1;
+            disc[u] = low[u] = time++;
+            int children = 0;
+            for(auto v : g[u]) if (v != p) {
+                if(vis[v]) {
+                    low[u] = min(low[u], disc[v]);
+                }
+                else {
+                    dfs1(v, u);
+                    low[u] = min(low[u], low[v]);
+                    if(low[v] >= disc[u] && p != -1) {
+                        ans.insert(names[u]);
+                    }
+                    children++;
+                }
+            }
+            if(p == -1 && children > 1) {
+                ans.insert(names[u]);
+            }
+        };
