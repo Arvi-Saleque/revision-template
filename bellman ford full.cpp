2ae348cbe0a87de2b8f9da3959370a7e137ffddf
Arvi_Saleque <alifsalek.as@gmail.com>
2025-05-21 23:10:32 +0600

    Update bellman ford full.cpp

--- bellman ford full.cpp
+++ bellman ford full.cpp
@@ -167,3 +167,80 @@ int main() {
     return 0;
 }
 
+
+// negatative cycle print
+
+#include <bits/stdc++.h>
+using namespace std; 
+typedef long long int ll;
+const int M = 2e5 + 9, mod = 1e9 + 7;
+const ll inf = 1e18;
+
+void solve() {
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<array<int, 3>> edges;
+
+    for(int i = 1; i <= m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({u, v, w});
+    }
+
+    vector<ll> dis(n + 1, inf);
+    vector<int> par(n + 1, -1);
+    int node = -1;
+
+    for(int i = 1; i <= n; i++) {
+        if(dis[i] != inf) continue;
+
+        dis[i] = 0;
+        for(int c = 1; c <= n; c++) {
+            bool relax = 1;
+            for(auto [u, v, w] : edges) {
+                if(dis[u] != inf && dis[u] + w < dis[v]) {
+                    dis[v] = dis[u] + w;
+                    par[v] = u;
+                    if(c == n) node = v;
+                    relax = 0;
+                }
+            }
+            if(relax) break;
+        }
+
+        if(node != -1) break;
+    }
+
+    if(node == -1) {
+        cout << "NO\n";
+    } 
+    else {
+        // Get a node in the cycle by walking back n times
+        for(int i = 0; i < n; i++) node = par[node];
+
+        vector<int> path;
+        int cur = node;
+        do {
+            path.push_back(cur);
+            cur = par[cur];
+        } while(cur != node);
+        path.push_back(node);
+        reverse(path.begin(), path.end());
+
+        cout << "YES\n";
+        for(int x : path) cout << x << " ";
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
