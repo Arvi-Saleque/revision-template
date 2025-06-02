3dfc6a170b915b6d184321576f4db6da2cfe717e
Arvi_Saleque <alifsalek.as@gmail.com>
2025-06-02 11:08:02 +0600

    Update palindrome construct.cpp

--- palindrome construct.cpp
+++ palindrome construct.cpp
@@ -53,3 +53,69 @@ int main(){
     while(t--) solve();
     return 0;
 }
+
+// number of pairs (u, v) a palindrome walk exist
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const int N = 1e7 + 9, mod = 1e9 + 7;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<char, int>>> g(n + 1), rg(n + 1);
+    for(int i = 1; i <= m; i++) {
+        int u, v; char w;
+        cin >> u >> v >> w;
+        g[u].push_back({w, v});
+        rg[v].push_back({w, u});
+    }
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+    for(int u = 1; u <= n; u++) {
+        dp[u][u]  = 1;
+        for(auto [w, v] : g[u]) {
+            dp[u][v] = 1;
+        }
+    }
+    deque<pair<int,int>> q;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(dp[i][j]) {
+                q.emplace_back(i, j);
+            }
+        }
+    }
+    while(!q.empty()) {
+        auto [v1, v2] = q.front();
+        q.pop_front();
+        for(auto [ch1, u1] : rg[v1]) {
+            for(auto [ch2, u2] : g[v2]) {
+                if(ch1 != ch2) continue;
+                if(!dp[u1][u2]) {
+                    dp[u1][u2] = 1;
+                    q.emplace_back(u1, u2);
+                }
+            }
+        }
+    }
+    int ans = 0;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(i == j) continue;
+            ans += dp[i][j];
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tc = 1, cs = 1;
+    while(tc--) {
+        solve();
+    }
+    return 0;
+}
+
